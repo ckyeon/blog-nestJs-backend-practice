6fbe76947afc477b8076631e90adec501a9b0922
ckyeon <qsky8520@gmail.com>
2022-06-28 12:16:07 +0900

    Add Config & Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PostsModule, CommentsModule, UsersModule],
+  imports: [
+    PostsModule,
+    CommentsModule,
+    UsersModule,
+
+    ConfigModule.forRoot({
+      envFilePath: [
+        join(
+          __dirname,
+          '../config',
+          `.${process.env.NODE_ENV || 'development'}.env`,
+        ),
+      ],
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
