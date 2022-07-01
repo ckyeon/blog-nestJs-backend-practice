1a1cf00ac19a7296ed55d825e3613cfa70edc087
ckyeon <qsky8520@gmail.com>
2022-07-02 01:17:11 +0900

    미들웨어 전역에서 활성화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [
     PostsModule,
     CommentsModule,
     UsersModule,
+    AuthModule,
 
     ConfigModule.forRoot({
-      envFilePath: [
-        join(
-          __dirname,
-          '../config',
-          `.${process.env.NODE_ENV || 'development'}.env`,
-        ),
-      ],
+      envFilePath: [join(__dirname, '../config', `.${process.env.NODE_ENV || 'development'}.env`)]
     }),
 
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
+        uri: configService.get<string>('MONGO_URI')
       }),
-      inject: [ConfigService],
-    }),
+      inject: [ConfigService]
+    })
   ],
   controllers: [],
-  providers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
